e9a2f97a070f774304c590dfbcad49322716a546
code0xff <ian.jungyong.um@gmail.com>
2024-01-01 23:14:45 +0900

    feat: Add template

--- client/zeromq/src/lib.rs
+++ client/zeromq/src/lib.rs
@@ -2,68 +2,61 @@ use futures::{SinkExt, StreamExt};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct Message {
-    topic: String,
-    message: serde_json::Value,
+	topic: String,
+	message: serde_json::Value,
 }
 
 #[derive(Clone)]
 pub struct MessageQueue {
-    pub sender: futures::channel::mpsc::Sender<Message>,
+	pub sender: futures::channel::mpsc::Sender<Message>,
 }
 
 impl MessageQueue {
-    pub fn new(sender: futures::channel::mpsc::Sender<Message>) -> Self {
-        Self { sender }
-    }
+	pub fn new(sender: futures::channel::mpsc::Sender<Message>) -> Self {
+		Self { sender }
+	}
 
-    pub async fn send(
-        mut self,
-        topic: &str,
-        message: &serde_json::Value,
-    ) -> Result<(), futures::channel::mpsc::SendError> {
-        let message = Message {
-            topic: topic.to_string(),
-            message: message.clone(),
-        };
-        self.sender.send(message).await
-    }
+	pub async fn send(
+		mut self,
+		topic: &str,
+		message: &serde_json::Value,
+	) -> Result<(), futures::channel::mpsc::SendError> {
+		let message = Message { topic: topic.to_string(), message: message.clone() };
+		self.sender.send(message).await
+	}
 }
 
 pub struct MessageQueueWorker {
-    sender: futures::channel::mpsc::Sender<Message>,
-    receiver: futures::channel::mpsc::Receiver<Message>,
-    socket: zmq::Socket,
+	sender: futures::channel::mpsc::Sender<Message>,
+	receiver: futures::channel::mpsc::Receiver<Message>,
+	socket: zmq::Socket,
 }
 
 impl MessageQueueWorker {
-    pub fn new(endpoint: &str, buffer: usize) -> Self {
-        let ctx = zmq::Context::new();
-        let socket = ctx.socket(zmq::PUB).unwrap();
-        socket.bind(endpoint).expect("socket failed to bind");
-        let (tx, rx) = futures::channel::mpsc::channel(buffer);
-        Self {
-            sender: tx,
-            receiver: rx,
-            socket,
-        }
-    }
+	pub fn new(endpoint: &str, buffer: usize) -> Self {
+		let ctx = zmq::Context::new();
+		let socket = ctx.socket(zmq::PUB).unwrap();
+		socket.bind(endpoint).expect("socket failed to bind");
+		let (tx, rx) = futures::channel::mpsc::channel(buffer);
+		Self { sender: tx, receiver: rx, socket }
+	}
 }
 
 impl MessageQueueWorker {
-    pub fn publish(&self, topic: &str, message: &str) -> Result<(), zmq::Error> {
-        self.socket.send(topic, zmq::SNDMORE)?;
-        self.socket.send(message, 0)?;
-        Ok(())
-    }
+	pub fn publish(&self, topic: &str, message: &str) -> Result<(), zmq::Error> {
+		self.socket.send(topic, zmq::SNDMORE)?;
+		self.socket.send(message, 0)?;
+		Ok(())
+	}
 
-    pub fn queue(&self) -> MessageQueue {
-        MessageQueue::new(self.sender.clone())
-    }
+	pub fn queue(&self) -> MessageQueue {
+		MessageQueue::new(self.sender.clone())
+	}
 
-    pub async fn run(mut self) {
-        while let Some(message) = self.receiver.next().await {
-            let Message { topic, message } = message;
-            let _ = self.publish(topic.as_str(), message.to_string().as_str());
-        }
-    }
+	pub async fn run(mut self) {
+		while let Some(message) = self.receiver.next().await {
+			let Message { topic, message } = message;
+			let _ = self.publish(topic.as_str(), message.to_string().as_str());
+		}
+	}
 }
--- example/src/main.rs
+++ example/src/main.rs
@@ -2,20 +2,21 @@ use zeromq::MessageQueueWorker;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let worker = MessageQueueWorker::new("tcp://127.0.0.1:13080", 32);
-    let queue = worker.queue();
+	let worker = MessageQueueWorker::new("tcp://127.0.0.1:13080", 32);
+	let queue = worker.queue();
 
-    tokio::spawn(async move {
-        loop {
-            let now = chrono::Local::now().to_string();
-            if let Err(e) = queue.clone().send("test-topic", &serde_json::Value::String(now)).await {
-                eprintln!("{:?}", e);
-            }
-            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-        }
-    });
+	tokio::spawn(async move {
+		loop {
+			let now = chrono::Local::now().to_string();
+			if let Err(e) = queue.clone().send("test-topic", &serde_json::Value::String(now)).await
+			{
+				eprintln!("{:?}", e);
+			}
+			tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+		}
+	});
 
-    worker.run().await;
+	worker.run().await;
 
-    Ok(())
+	Ok(())
 }
